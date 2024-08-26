77024c429dc7f820c56795a1210abd38552aa486
GenaDeev <genaroelgenio@hotmail.com>
2024-08-25 23:41:20 -0300

    💫🖼️ Optimized GenaDeev logo to webp and updated its references

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
 
     return (
         <footer className="lg:ml-[256px] gap-4 rounded-t-xl flex items-center justify-center">
-            <img width={32} src="/genadev-v-nobg-1.png" alt="Logo de GenaDeev"/>
+            <img width={32} src="/genadev-v-nobg-1.webp" alt="Logo de GenaDeev"/>
             <p>© {currentYear()} Desarrollado por <Link className="hover:text-green-500 transition duration-200" to="https://github.com/GenaDeev">GenaDeev</Link> y mantenido por <Link className="hover:text-green-500 transition duration-200" to="https://comidolar.com.ar">ComiDolar</Link></p>
             <img width={64} src="/comidolar-logo.webp" alt="Logo de ComiDolar"/>
         </footer>
