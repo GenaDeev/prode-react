c9e58706a927a542ca61fc51807105429c3146d1
GenaDeev <genaroelgenio@hotmail.com>
2024-08-25 23:46:29 -0300

    🖼️🐌 Added lazy load to team images

--- src/components/TeamComponent.tsx
+++ src/components/TeamComponent.tsx
@@ -9,7 +9,7 @@ export default function TeamComponent(props: Props) {
     const {shortName, fullName, id, goals} = props;
     return (
         <div className="flex flex-col items-center">
-            <img width="64px" height="64px" className="object-contain aspect-square" src={"/escudos/" + id + ".svg"} />
+            <img loading="lazy" width="64px" height="64px" className="object-contain aspect-square" src={"/escudos/" + id + ".svg"} />
             <span title={fullName} className="font-semibold">{shortName}</span>
             <span className="text-xl font-bold bg-neutral-200 px-3 py-1 mt-2">{goals ?? '-'}</span>
         </div>
